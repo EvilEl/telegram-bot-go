be7e3a974cee1903d6971326a4875ccee22ee343
Михаил Андреев <mihailandreev@MacBook-Pro-Mihail.local>
2024-11-24 23:26:20 +0300

    feat:added commands telegram bot

--- handlers/bot/bot.go
+++ handlers/bot/bot.go
@@ -9,19 +9,28 @@ import (
 )
 
 func CreateBot() {
+
 	config := config.New()
 	fmt.Println("CreateBot:config.TELEGRAM_APITOKEN", config.TELEGRAM_APITOKEN)
 	bot, err := tgbotapi.NewBotAPI(config.TELEGRAM_APITOKEN)
+
 	if err != nil {
+		panic(err)
+	}
+	err = CreateCommands(bot)
+
+	if err != nil {
+		log.Fatalf("Ошибка установки команд: %v", err)
 		panic(err)
 	}
 
 	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
 
 	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
+
+		fmt.Println(update.Message)
 		if update.Message == nil { // ignore any non-Message updates
 			continue
 		}
@@ -29,11 +38,9 @@ func CreateBot() {
 		if !update.Message.IsCommand() { // ignore any non-command Messages
 			continue
 		}
-
 		// Create a new MessageConfig. We don't have text yet,
 		// so we leave it empty.
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-
 		// Extract the command from the Message.
 		switch update.Message.Command() {
 		case "help":
--- handlers/bot/commands.go
+++ handlers/bot/commands.go
@@ -1,1 +1,22 @@
 package bot
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+var botCommand = []tgbotapi.BotCommand{
+	{
+		Command:     "help",
+		Description: "Помощь",
+	},
+	{
+		Command:     "status",
+		Description: "Получить статус",
+	},
+}
+
+func CreateCommands(bot *tgbotapi.BotAPI) error {
+	setCommandsConfig := tgbotapi.NewSetMyCommands(botCommand...)
+	_, err := bot.Request(setCommandsConfig)
+	return err
+}
